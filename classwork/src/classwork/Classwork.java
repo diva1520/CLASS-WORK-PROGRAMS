a375391e4a433ea1033a4ab4f5a28ea4fb3724b9
Thivagar <thivagarr1520@gmail.com>
2025-08-20 17:48:01 +0530

    my first class work

--- classwork/src/classwork/Classwork.java
+++ classwork/src/classwork/Classwork.java
@@ -15,7 +15,7 @@ public class Classwork {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hii");
     }
     
 }
